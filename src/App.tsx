e396f29c4c2a046490fc040f3e850025857594d6
Aanandita Gupta <aananditagupta14@gmail.com>
2023-10-08 22:07:06 +0100

    feat: added bus list & clock component

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import BusList from './components/BusList';
+import Clock from './components/Clock';
 
-function App() {
+// Example bus services data
+const services = [
+  { name: 'X15', destination: 'SHEFFIELD', interval: 60, start: 0, end: 1439 },
+  { name: '52', destination: 'WAKEFIELD', interval: 12, start: 360, end: 1260 },
+  { name: 'X78', destination: 'DONCASTER', interval: 20, start: 0, end: 1439 },
+  { name: '126', destination: 'LEEDS', interval: 6, start: 330, end: 90 },
+];
+
+// App component
+const App = () => {
+  // For the purpose of this example, let's assume currentTime is 720 (12:00 PM)
+  const currentTime = 720;
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* Use the new Clock component to display the current local time */}
+      <Clock />
+
+      {/* Use BusList to display the bus services */}
+      <BusList services={services} currentTime={currentTime} />
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/Clock.tsx
@@ -0,0 +1,35 @@
+import React, { useEffect, useState } from 'react';
+
+// Clock component to show the current time in hh:mm:ss format
+const Clock = () => {
+  const [currentTime, setCurrentTime] = useState(new Date());
+
+  const updateTime = () => {
+    setCurrentTime(new Date());
+  };
+
+  useEffect(() => {
+    const timerID = setInterval(updateTime, 1000);
+    return () => {
+      clearInterval(timerID);
+    };
+  }, []);
+
+  const hours = currentTime.getHours();
+  const minutes = currentTime.getMinutes();
+  const seconds = currentTime.getSeconds();
+
+  const formattedHours = String(hours).padStart(2, '0');
+  const formattedMinutes = String(minutes).padStart(2, '0');
+  const formattedSeconds = String(seconds).padStart(2, '0');
+
+  return (
+    <div className="clock">
+      <h1>
+        {formattedHours}:{formattedMinutes}:{formattedSeconds}
+      </h1>
+    </div>
+  );
+};
+
+export default Clock;
