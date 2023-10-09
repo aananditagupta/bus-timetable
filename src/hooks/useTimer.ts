fa4f3757c6ffa28ece7946271d2a42a33faece09
Aanandita Gupta <aananditagupta14@gmail.com>
2023-10-09 10:13:32 +0100

    docs: updated comments within code

--- src/hooks/useTimer.ts
+++ src/hooks/useTimer.ts
@@ -4,7 +4,9 @@ export const useTimer = () => {
   const [time, setTime] = useState<number>(0);
 
   useEffect(() => {
+    // Create an interval that increments 'time' every 1000 milliseconds (1 second)
     const interval = setInterval(() => {
+      // Update the 'time' state with the new incremented value
       setTime(prevTime => prevTime + 1);
     }, 1000);
 
